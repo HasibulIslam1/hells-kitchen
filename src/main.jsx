06c1abc433b3af81ef1f7789e65f7f14bad9cfd5
HasibulIslam1 <hasibshanto143@gmail.com>
2023-10-29 23:30:15 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import './index.css'
+import React from 'react';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
